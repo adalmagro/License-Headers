7a76dae58d3c72f7b9cb8e689472c8b9d344c9b7
arturo <arturo.diaz@urjc.es>
2010-12-01 12:53:22 +0100

    Added license header to all project files.

--- src/addLicenseHead.sh
+++ src/addLicenseHead.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 #
+# Copyright 2010 Arturo Díaz-Almagro
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+# You should have received a copy of the GNU General Public License
+# along with this project.  If not, see <http://www.gnu.org/licenses/>.
+
+
+#
 # This script adds the GPLv3 header to all files within
 # the directory passed as parameter.
 #
